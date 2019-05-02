44212fc70fa96dc3047ba7a9a6614b890d16b486
Loop421 <lopez.e421@gmail.com>
2019-05-02 17:30:30 -0400

    Update DoublyLinkedList.js

--- JavaScript/DoublyLinkedList.js
+++ JavaScript/DoublyLinkedList.js
@@ -42,5 +42,36 @@ class DoublyLinkedList
     this.length++;
     return this;
   }
+  pop()
+  {
+    /*
+      If there is no head, return undefined
+      Store the current tail in variable to return later
+      If the length is 1, set the head and tail to be null
+      Otherwise update the tail to be the previous Node
+      Set the new tail's next to null
+      Set the old tail previous to be null
+      Decrement the length
+      Return the value removed
+    */
+    if(!this.head) return undefined;
+
+    var oldTail = this.tail; 
+
+    if(this.length == 1)
+    {
+      this.head = null;
+      this.tail = null;
+    }
+    else
+    {
+      this.tail = oldTail.prev;
+      this.tail.next = null
+      oldTail.prev = null;
+    }
+    this.length--;
+    return oldTail;
+    
+  }
 
 }
