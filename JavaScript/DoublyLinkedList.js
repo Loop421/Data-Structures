5f2c65edcc0f42568d5d8f8be5714fed52a65e3f
Loop421 <lopez.e421@gmail.com>
2019-05-16 16:06:48 -0400

    Update DoublyLinkedList.js

--- JavaScript/DoublyLinkedList.js
+++ JavaScript/DoublyLinkedList.js
@@ -235,5 +235,32 @@ class DoublyLinkedList
     return true;
 
   }
+    
+  remove(index)
+  {
+    /*
+      *If the index is less than zero or greater than or equal to the lenght return undefined
+      *If the index is 0, shift
+      *If the index is the same as the length - 1, pop
+      *Use the get method to retrieve the item to be remove the found node from the list
+      *Set next and prev to null on the found node
+      *Decrement the length
+      *Return the removed node
+    */
+
+    if(index < 0 || index > this.length) return undefined;
+    if(index == 0) return this.shift();
+    if(index == this.length - 1) return this.pop();
+
+    var removeNode = this.get(index);
+    removeNode.prev.next = removeNode.next;
+    removeNode.next.prev = removeNode.prev;
+    
+    removeNode.next = null;
+    removeNode.prev = null;
+    this.length--;
+    return removeNode;
+
+  }
 
 }
