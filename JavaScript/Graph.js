dc974025347a284e7b76ddb9af8182d35ba65949
Loop421 <lopez.e421@gmail.com>
2019-06-11 15:52:58 -0400

    Update Graph.js

--- JavaScript/Graph.js
+++ JavaScript/Graph.js
@@ -55,4 +55,80 @@ class Graph
 
     delete this.adjacencyList[vertex];
   }
+  depthFirstRecursive(start)
+  {
+    /*
+      -The function should accept a starting node
+      -Create a list to store the end result, to be returned at the very end
+      -Create an object to store visited vertices
+      -Create a helper function which accept a vertex
+        -The helper function should return early if the vertex is empty
+        -The helper function should place the vertex it accepts into the
+         visited object and push that vertex into the result array
+        -Loop over all of the values in the adjacencyList for that vertex
+        -if any of those values have not been visited, recursively invoke
+         the helper function with that vertex
+      -invoke the helper function with starting vertex
+      -Return the result array
+    */
+    let result = [];
+    const visited = {};
+    const adjacencyList = this.adjacencyList;
+
+    (function dfs(vertex)
+    {
+      if(!vertex) return null;
+      visited[vertex] = true;
+      result.push(vertex);
+      adjacencyList[vertex].forEach(neighbor =>
+      {
+        if(!visited[neighbor])
+        {
+          return dfs(neighbor)
+        }
+      });
+    })(start);
+
+    return result;
+    
+  }
+  depthFirstIterative(start)
+  {
+    /*
+      -The function should accept a starting node
+      -Create a stack to help use keep track of vertices(use a list/array)
+      -Create a list to store the end result, to be returned at the very end
+      -Create an object to store visited vertices
+      -Add the starting vertex to the stack, and mark it visited
+      -While the stack has something in it:
+        -Pop the next vertex from the stack
+        -Add it to the result list
+        -If that vertex hasn't been visited yet:
+          -Mark it as visited
+          -Push all of it neighbors into the stack
+      -Return the result array
+    */
+    const stack = [start];
+    const result = [];
+    const visited = {};
+    let currentVertex;
+
+    visited[start] = true;
+
+    while(stack.length)
+    {
+      currentVertex = stack.pop();
+      result.push(currentVertex);
+      
+      this.adjacencyList[currentVertex].forEach(neighbor =>
+      { 
+        if(!visited[neighbor])
+        {
+          visited[neighbor] = true;
+          stack.push(neighbor);
+        }
+      });
+    }
+    return result;
+  }
 }
